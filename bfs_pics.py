1461707f05b3f4932e9dc468c3a7b70c2e9b1327
Michael Brooks <mjbrooks@uw.edu>
2013-10-28 20:21:49 -0700

    add html output

--- bfs_pics.py
+++ bfs_pics.py
@@ -10,7 +10,9 @@ nodes are a third color.
 import logging
 import os
 import math
+from datetime import datetime
 from PIL import Image, ImageDraw
+from string import Template
 
 import common
 from catdb import models
@@ -70,7 +72,7 @@ def update_image(image, categories, prev_categories):
     if len(categories):
         draw.point([map_category(cat, image) for cat in categories], frontier_color)
 
-def save_image(path, iteration_name, image):
+def save_image(output_dir, version_path, iteration_name, image):
     """
     Saves the image into the specified path, obviously.
     :param path:
@@ -78,9 +80,103 @@ def save_image(path, iteration_name, image):
     :param image:
     :return:
     """
-    fname = os.path.join(path, "%s.png" % iteration_name)
-    with open(fname, 'wb') as outfile:
+    fname = os.path.join(version_path, "%s.png" % iteration_name)
+    full_path = os.path.join(output_dir, fname)
+    with open(full_path, 'wb') as outfile:
         image.save(outfile)
+        print "Saved %s" % full_path
+
+    return fname
+
+def save_index(path, version_images, root_category, max_depth):
+    """
+    Generates an HTML file that will display all the images.
+
+    version_images should be a list of tuples where the first element
+    is a version-identifier and the second is a list of (paths to images, depth)
+
+    For example:
+    version_images = [
+        ("v1", [("1/1.png", 1), ("1/2.png", 2)]),
+        ("v2", [("2/1.png", 1), ("2/2.png", 2)]),
+    ]
+
+    path should be a path to save the index file into.
+
+    :param path:
+    :param version_images:
+    :return:
+    """
+
+    outer_template = Template("""
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+        <title>$root subcategories</title>
+        <!-- Generated $today -->
+        <!-- Depth limit: $max_depth -->
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"/>
+        <style>
+        body {
+            margin: 20px;
+        }
+        .images {
+            white-space: nowrap;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>$root subcategories</h1>
+        <p class="muted">Generated $today. Depth limited to $max_depth.</p>
+        <p>From the root category ($root), subcategories were explored in a breadth-first fashion.
+        Each image, from left to right, reveals another level of the hierarchy.
+        Each pixel corresponds to a single category. They are ordered by their database id, which may be arbitrary (or not).
+        Just-traversed categories are in green, while previously visited categories are in orange.
+        DBpedia versions are separated vertically.
+
+        <div class="images">
+        $versions
+        </div>
+    </body>
+    </html>
+    """)
+
+    version_template = Template("""
+    <div class="version">
+        <h2>$version</h2>
+        <div class="images">
+        $images
+        </div>
+    </div>
+    """)
+
+    image_template = Template("""
+    <img src="$src" title="depth $depth for version $version"/>
+    """)
+
+    indexfile = os.path.join(path, 'index.html')
+    with open(indexfile, 'wt') as outfile:
+
+        versions = []
+        for version_key, image_list in version_images:
+            images = []
+
+            for img_path, depth in image_list:
+                images.append(
+                    image_template.substitute(src=img_path, depth=depth, version=version_key)
+                )
+
+            images = "".join(images)
+            versions.append(
+                version_template.substitute(version=version_key, images=images)
+            )
+
+        versions = "".join(versions)
+        html = outer_template.substitute(root=root_category, max_depth=max_depth, today=str(datetime.now()), versions=versions)
+        outfile.write(html)
+
+    print "Saved %s" % indexfile
+
 
 def bfs_pics(root_name, depth, output_dir, db):
     models.database_proxy.initialize(db)
@@ -89,9 +185,15 @@ def bfs_pics(root_name, depth, output_dir, db):
 
     versions = DataSetVersion.select()
 
+    version_images = []
     for version in versions:
+        print "Generating images for version %d..." % (version.id)
+
+        current_version_images = []
+
         # make the output directory
-        dirname = os.path.join(output_dir, str(version.id))
+        version_path = str(version.id)
+        dirname = os.path.join(output_dir, version_path)
 
         try:
             os.makedirs(dirname)
@@ -114,8 +216,11 @@ def bfs_pics(root_name, depth, output_dir, db):
         for cat in descendants:
             level = descendants.current_level
             if level != current_level:
+                print "Traversed %d categories at level %d" % (len(frontier), current_level)
+
                 update_image(image, frontier, prev_frontier)
-                save_image(dirname, current_level, image)
+                fname = save_image(output_dir, version_path, current_level, image)
+                current_version_images.append((fname, current_level))
 
                 prev_frontier = frontier
                 frontier = []
@@ -124,9 +229,17 @@ def bfs_pics(root_name, depth, output_dir, db):
             frontier.append(cat)
 
         if len(frontier):
+            print "Traversed %d categories at level %d" % (len(frontier), current_level)
+
             # any last few to save
             update_image(image, frontier, prev_frontier)
-            save_image(dirname, current_level, image)
+            fname = save_image(output_dir, version_path, current_level, image)
+            current_version_images.append((fname, current_level))
+
+        version_key = "%d: %s (%s)" % (version.id, version.version, str(version.date))
+        version_images.append((version_key, current_version_images))
+
+    save_index(output_dir, version_images, root_name, depth)
 
 if __name__ == "__main__":
     import argparse
